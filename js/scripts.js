61d0875e683dec805be09721a2060b6d172015cc
Mary Robin Rose <maryrobinrose@gmail.com>
2019-04-20 18:13:07 -0700

    update ajax function

--- js/scripts.js
+++ js/scripts.js
@@ -8,15 +8,15 @@ FSJS project 5 - Public API Requests
 //Use AJAX to call the Random User Generator API
 function ajax() {
     $.ajax({
-      url: 'https://randomuser.me/api/',
+      url: 'https://randomuser.me/api/?results=12',
       dataType: 'json',
       success: function(data) {
         console.log(data);
+        fetchData(data);
       }
     });
 }
 ajax();
-
 //Call function?^
 
 
@@ -31,24 +31,51 @@ const search = `<form action="#" method="get">
 document.querySelector('.search-container').innerHTML = search;
 
 //*** Users ***//
-//Get and display 12 random users
-//Select HTML markup from index.html
-const gallery = `<div class="card">
-    <div class="card-img-container">
-        <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-    </div>
-    <div class="card-info-container">
-        <h3 id="name" class="card-name cap">first last</h3>
-        <p class="card-text">email</p>
-        <p class="card-text cap">city, state</p>
-    </div>
-</div>`;
+//Get and display users
+function fetchData(data) {
+  //Loop over Users
+  $.each(data.items,function() {
+    //Select HTML markup from index.html
+    const gallery = `<div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">first last</h3>
+            <p class="card-text">email</p>
+            <p class="card-text cap">city, state</p>
+        </div>
+    </div>`;
 
-//Append to the page
-document.querySelector('.gallery').innerHTML = gallery;
+    //Append to the page
+    document.querySelector('.gallery').innerHTML = gallery;
+  }); //end each
+}
+
+  /*fetch('https://randomuser.me/api/')
+    .then(checkStatus)
+    //Takes response object and converts it to JSON (Promise)
+    .then(response => response.json())
+    //Catches a return error
+    .catch(error => console.log('Looks like there was a problem', error))
+}
+
+//Helper Function
+function checkStatus(response) {
+  //If the response comes back working
+  if (response.ok) {
+    return response.resolve()
+  } else {
+    //If the response fails
+    return response.reject(new Error(response.statusText));
+  }*/
+
+
+
+
 
 //Create modal window
-/*<div class="modal-container">
+const modal = `<div class="modal-container">
     <div class="modal">
         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
         <div class="modal-info-container">
@@ -61,33 +88,5 @@ document.querySelector('.gallery').innerHTML = gallery;
             <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
             <p class="modal-text">Birthday: 10/21/2015</p>
         </div>
-    </div>*/
+    </div>`;
     //Make sure there’s a way to close the modal window
-
-
-
-
-/*var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-        document.getElementById('ajax').innerHTML = xhr.responseText;
-        } else if (xhr.status === 404) {
-          //file not found
-        } else if (xhr.status = 500) {
-        //server had a problem
-        }
-      }
-    };
-    xhr.open('GET', 'sidebar.html');
-    function sendAJAX() {
-      xhr.send();
-      document.getElementById('load').style.display = 'none';
-    }
-
-    <button id='load' onclick='sendAJAX()'>Bring It!</button>
-          </div>
-          <div id="ajax">
-
-          </div>
-        </div>*/
