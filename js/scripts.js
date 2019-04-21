cc3ed5269efeda2b8662daf73c8b587afafc1414
Mary Robin Rose <maryrobinrose@gmail.com>
2019-04-20 20:51:28 -0700

    add template strings to modal

--- js/scripts.js
+++ js/scripts.js
@@ -65,14 +65,14 @@ function makeModal(data) {
       <div class="modal">
           <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
           <div class="modal-info-container">
-              <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-              <h3 id="name" class="modal-name cap">name</h3>
-              <p class="modal-text">email</p>
-              <p class="modal-text cap">city</p>
+              <img class="modal-img" src="${data.results[i].picture.large}" alt="${data.results[i].name.first} ${data.results[i].name.last}">
+              <h3 id="name" class="modal-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
+              <p class="modal-text">${data.results[i].email}</p>
+              <p class="modal-text cap">${data.results[i].location.city}</p>
               <hr>
-              <p class="modal-text">(555) 555-5555</p>
-              <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-              <p class="modal-text">Birthday: 10/21/2015</p>
+              <p class="modal-text">${data.results[i].phone}</p>
+              <p class="modal-text">${data.results[i].location.street}, ${data.results[i].location.city}, ${data.results[i].location.state} ${data.results[i].location.postcode}</p>
+              <p class="modal-text">Birthday: ${data.results[i].dob.date}</p>
           </div>
       </div>`;
 
